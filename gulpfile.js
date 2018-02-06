8e0c5172a4b2f0adcd88ecb401722b0dc586f91e
Wizard67 <wizard67.hjn@gmail.com>
2018-02-06 15:48:50 +0800

    [init]

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+gulp.task('css', function () {
+    return gulp.src('./scss/base.scss')
+               .pipe(sass().on('error', sass.logError))
+               .pipe(rename(function(path){
+                   path.basename = 'style'
+               }))
+               .pipe(gulp.dest('./app/css/'))
+})
+
+gulp.task('css:watch', function () {
+    gulp.watch('./scss/**/*.scss', ['css'])
+})
+
+gulp.task('serve', ['css:watch'], function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        }
+    })
+
+    gulp.watch(['*.html', 'css/*.css', 'js/*.js', 'imgs/**/*.*'], {cwd: 'app'}, reload)
+});
