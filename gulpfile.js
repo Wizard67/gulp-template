19f33ded4a486c80686be70193ce84c031be096a
Wizard67 <wizard67.hjn@gmail.com>
2018-04-24 15:25:18 +0800

    add: autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,43 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const rename = require('gulp-rename');
-const browserSync = require('browser-sync');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const rename = require("gulp-rename");
+const browserSync = require("browser-sync");
+const autoprefixer = require("gulp-autoprefixer");
 const reload = browserSync.reload;
 
-gulp.task('css', function () {
-    return gulp.src('./scss/base.scss')
-               .pipe(sass().on('error', sass.logError))
-               .pipe(rename(function(path){
-                   path.basename = 'style'
-               }))
-               .pipe(gulp.dest('./app/css/'))
-})
+gulp.task("css", function() {
+  return gulp
+    .src("./scss/base.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions", "Android >= 4.0"],
+        cascade: true,
+        remove: true
+      })
+    )
+    .pipe(
+      rename(function(path) {
+        path.basename = "style";
+      })
+    )
+    .pipe(gulp.dest("./app/css/"));
+});
 
-gulp.task('css:watch', function () {
-    gulp.watch('scss/**/*.scss', ['css'])
-})
+gulp.task("css:watch", function() {
+  gulp.watch("scss/**/*.scss", ["css"]);
+});
 
-gulp.task('serve', ['css:watch'], function() {
-    browserSync({
-        server: {
-            baseDir: 'app'
-        }
-    })
+gulp.task("serve", ["css:watch"], function() {
+  browserSync({
+    server: {
+      baseDir: "app"
+    }
+  });
 
-    gulp.watch(['**/*.html', 'css/*.css', 'js/*.js', 'imgs/**/*.*'], {cwd: 'app'}, reload)
+  gulp.watch(
+    ["**/*.html", "css/*.css", "js/*.js", "imgs/**/*.*"],
+    { cwd: "app" },
+    reload
+  );
 });
