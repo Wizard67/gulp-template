dc13751954643af16472494212a58859984c0d30
Wizard67 <wizard67.hjn@gmail.com>
2018-05-04 10:46:26 +0800

    add: sass-glob

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 const gulp = require("gulp");
-const plumber = require('gulp-plumber');
 const sass = require("gulp-sass");
 const pug = require('gulp-pug');
+const plumber = require('gulp-plumber');
+const sassGlob = require('gulp-sass-glob');
 const rename = require("gulp-rename");
 const browserSync = require("browser-sync");
 const autoprefixer = require("gulp-autoprefixer");
@@ -10,8 +11,8 @@ const reload = browserSync.reload;
 gulp.task("scss", function () {
   return gulp
     .src("./scss/base.scss")
-    .pipe(plumber())
-    .pipe(sass().on("error", sass.logError))
+    .pipe(sassGlob())
+    .pipe(sass(/*{ outputStyle: "compressed" }*/).on("error", sass.logError))
     .pipe(
       autoprefixer({
         browsers: ["last 2 versions", "Android >= 4.0"],
